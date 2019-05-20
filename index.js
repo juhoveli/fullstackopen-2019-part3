1a7699fdbbe49946d5ade30ee2feb23ea630d437
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-20 11:24:32 +0300

    puhelinluettelon backend step1

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
 const express = require('express')
 const app = express()
 
-let notes = [
+let persons = [
   {
-    id: 1,
-    content: 'HTML on helppoa',
-    date: '2017-12-10T17:30:31.098Z',
-    important: true,
+    name: "Arto Hellas",
+    number: "040-123456",
+    id: 1
   },
   {
-    id: 2,
-    content: 'Selain pystyy suorittamaan vain javascriptiä',
-    date: '2017-12-10T18:39:34.091Z',
-    important: false,
+    name: "Martti Tienari",
+    number: "040-123456",
+    id: 2
   },
   {
-    id: 3,
-    content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-    date: '2017-12-10T19:20:14.298Z',
-    important: true,
+    name: "Arto Järvinen",
+    number: "040-123456",
+    id: 3
   },
+  {
+    name: "Lea Kutvonen",
+    number: "040-123456",
+    id: 4
+  }
 ]
 
-app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
-})
-
-app.get('/notes', (req, res) => {
-  res.json(notes)
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
 })
 
 const PORT = 3001
