a9aca4150ac7d1413bfcec6a65c59164f543a71c
Juho Salminen <juho.salminen@Juho-MacBook-Pro.local>
2019-05-21 17:33:51 +0300

    puhelinluettelon backend step11

---
+++ build/precache-manifest.93d215ca9e313b91524eee113e65e9f1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "649610b242043593bd6d2ab54cca93fa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3005de686b0f6ccf601",
+    "url": "/static/css/main.0964f825.chunk.css"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "b3005de686b0f6ccf601",
+    "url": "/static/js/main.ea0300a1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
---
+++ build/precache-manifest.d554cac1d0368d163cd78176166d3505.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24203caac41eefcda898d52aead67e15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "997fd5b956a3ddabf049",
+    "url": "/static/css/main.0964f825.chunk.css"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "997fd5b956a3ddabf049",
+    "url": "/static/js/main.81a94e27.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
